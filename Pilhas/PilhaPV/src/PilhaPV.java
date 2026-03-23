b70b5a00026cfe4669f84e58057de9dc413a562b
Ryan Almeida <ryanalds21@gmail.com>
2026-03-23 11:41:05 -0300

    implementação PilhaPV

--- Pilhas/PilhaPV/src/PilhaPV.java
+++ Pilhas/PilhaPV/src/PilhaPV.java
@@ -8,7 +8,7 @@ public class PilhaPV {
         this.capacidade = capacidade;
         this.pilha = new Object[capacidade];
         this.topoVermelho = -1;
-        this.topoPreto = capacidade + 1;
+        this.topoPreto = capacidade;
     }
 
 
@@ -17,7 +17,7 @@ public class PilhaPV {
     }
 
     public boolean isEmpty() {
-        if (topoVermelho == -1 && topoPreto == capacidade + 1) {
+        if (topoVermelho == -1 && topoPreto == capacidade ) {
             return true;
         }
         return false;
@@ -32,21 +32,63 @@ public class PilhaPV {
     }
 
     public void pushVermelho(Object o) {
-        if (topoVermelho + 1 == topoPreto) {
-            throw new RuntimeException("Pilha Cheia");
-
+        if(topoVermelho + 1 == topoPreto){
+            redimencionar(capacidade * 2);
         }
         pilha[++topoVermelho] = o;
     }
 
     public void pushPreto(Object o) {
-        if (topoVermelho + 1 == topoPreto) {
-            throw new RuntimeException("Pilha Cheia");
+        if(topoVermelho + 1 == topoPreto){
+            redimencionar(capacidade * 2);
         }
-        pilha[--topoPreto] = o;
+        pilha[++topoPreto] = o;
     }
 
-    public Object pop() throws PilhaVaziaExcecao {
-        return null;
+    public Object popVermelho() throws PilhaVaziaExcecao {
+        if (isEmpty()) {
+            throw new PilhaVaziaExcecao("Pilha Vazia");
+        }
+        Object elemento = pilha[topoVermelho];
+        pilha[topoVermelho--] = null;
+        topoVermelho--;
+        verificarReducao();
+        return elemento;
+    }
+    public Object popPreto() throws PilhaVaziaExcecao {
+        if (isEmpty()) {
+            throw new PilhaVaziaExcecao("Pilha Vazia");
+        }
+        Object elemento = pilha[topoPreto];
+        pilha[topoPreto] = null;
+        topoPreto++;
+        verificarReducao();
+        return elemento;
+    }
+
+    private void redimencionar(int novaCapacidade) {
+        if (novaCapacidade < size() + 1) {
+            novaCapacidade = size() + 1;
+        }
+
+        Object[] novaPilha = new Object[novaCapacidade];
+        for (int i = 0; i < novaPilha.length; i++) {
+            novaPilha[i] = pilha[i];
+        }
+
+        int novoPreto =  novaCapacidade;
+        for(int i = capacidade - 1 ; i >= topoPreto; i--) {
+            novaPilha[--novoPreto] = pilha[i];
+        }
+        this.pilha = novaPilha;
+        this.topoPreto = novoPreto;
+        this.capacidade = novaCapacidade;
+    }
+
+    private void verificarReducao() {
+        int novaCapacidade = capacidade/2;
+        if(size() <= capacidade/3 && novaCapacidade >= size() + 1 ){
+            redimencionar(novaCapacidade);
+        }
     }
 }
